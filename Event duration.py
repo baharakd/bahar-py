a1dae0d0d84102f36f57d44b2fcc70fa3595772b
Bahare <bahareakhoondi@gmail.com>
2019-07-02 11:23:39 +0430

    S Durations

--- Event duration.py
+++ Event duration.py
@@ -44,6 +44,33 @@ print("\n")
 for k in range(len(data)-1):
     print("event number", k + 1, "is", dur_lst[k], "it's begins at:", data[k], "and ends at", data[k + 1])
 
-
+print("\n")
 
 #3.3- مدت زمان توالی S ها
+new_data = [200, 800, 900, 1500, 2000, 2200, 2400, 2900, 3400, 3600, 3800, 4500, 5000, 5200
+            , 5400, 5600, 5800, 6500, 7000, 7200, 8000]
+new_dur = [600, 100, 600, 500, 200, 200, 500, 500, 200, 200, 700, 500, 200, 200, 200, 200, 700, 500, 200, 800]
+new_dur_str = "LSLLSSLLSSSLLSSSSLLSL"
+s_seq = 0
+for p in range(len(new_dur_str)-1):
+    if new_dur_str[p] == "L" and new_dur_str[p+1] == "S" and new_dur_str[p+2] == "L":
+        s_seq = new_data[p+2] - new_data[p+1]
+        print("S duration in LSL is:",s_seq)
+for p in range(len(new_dur_str)-1):
+    if new_dur_str[p] == "L" and new_dur_str[p+1] == "S" and new_dur_str[p+2] == "S" and new_dur_str[p+3] == "L":
+        s_seq = new_data[p+3] - new_data[p+1]
+        print("S duration in LSSL is:",s_seq)
+for p in range(len(new_dur_str)-1):
+    if new_dur_str[p] == "L" and new_dur_str[p+1] == "S" and new_dur_str[p+2] == "S" and new_dur_str[p+3] == "S"\
+            and new_dur_str[p+4] == "L":
+        s_seq = new_data[p+4] - new_data[p+1]
+        print("S duration in LSSSL is:",s_seq)
+for p in range(len(new_dur_str) - 1):
+    if new_dur_str[p] == "L" and new_dur_str[p + 1] == "S" and new_dur_str[p + 2] == "S" and new_dur_str[p + 3] == "S" \
+            and new_dur_str[p+4] == "S" and new_dur_str[p + 5] == "L":
+        s_seq = new_data[p + 5] - new_data[p + 1]
+        print("S duration in LSSSSL is:", s_seq)
+
+
+
+
