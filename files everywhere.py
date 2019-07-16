087b39cad013f9df9c7e35210ec63f2b926c8bb1
Bahare <bahareakhoondi@gmail.com>
2019-07-16 10:47:07 +0430

    files everywhere

--- files everywhere.py
+++ files everywhere.py
@@ -35,4 +35,20 @@ for i in range(len(no_child_list)):
 
 #طولانی ترین مسیر ممکن را پیدا کنید و تمام اعضایی که در مسیر آن دیده شده را در فایل longest_path.txt ذخیره کنید.
 
+def find_parents_children(key):
+    dic_p ={}
+    lst_ch = []
+    for f in list_of_files:
+        inside = open(f,mode="r").read()
+        if inside == key:
+            lst_ch.append(inside)
+    dic_p.update({key:lst_ch})
+
+    return dic_p
+
+print(find_parents_children("1204674588"))
+
+#for i in range(len(list_of_no_parents)):
+ #   dic_key = list_of_no_parents[i]
+  #  dic_of_parents = find_parents_children(dic_key)
 
