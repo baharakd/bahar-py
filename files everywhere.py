915ba0be6c37f9dae36680ed67168ec828878f22
Bahare <bahareakhoondi@gmail.com>
2019-07-16 09:59:08 +0430

    files everywhere

--- files everywhere.py
+++ files everywhere.py
@@ -23,7 +23,7 @@ print("list of files that has no parent:", list_of_no_parents)
 #لیستی از تمامی فایل‌هایی که هیچ childی ندارد را (به صورت sort شده) در فایل no_children.txt ذخیره کنید
 
 def difference (list1, list2):
-   list_dif = [i for i in list1 + list2 if i not in list1 or i not in list2]
+   list_dif = [i for i in list1 + list2 if i not in list2]
    return list_dif
 
 no_child_list = difference(child_lst,parent_lst)
@@ -32,3 +32,7 @@ no_children = open("no_children.txt", mode="a")
 for i in range(len(no_child_list)):
     no_children.write(no_child_list[i])
     no_children.write("\n")
+
+#طولانی ترین مسیر ممکن را پیدا کنید و تمام اعضایی که در مسیر آن دیده شده را در فایل longest_path.txt ذخیره کنید.
+
+
